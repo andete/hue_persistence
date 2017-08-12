7d0e063e40a8ac70e53d14d508f57fcafb666b89
Joost Yervante Damad <joost@damad.be>
2017-08-12 12:49:20 +0200

    switch to philipshue using Hyper 0.11/Futures implementing timeout for bridge request (Closes #6)

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,25 @@ extern crate log;
 extern crate error_chain;
 extern crate clap;
 extern crate env_logger;
+extern crate futures;
+extern crate tokio_timer;
 
 use std::env;
+use std::time::Duration;
 use std::collections::HashMap;
 use philipshue::bridge::{Bridge, discover_upnp};
 use philipshue::hue::LightCommand;
+use philipshue::network::Core;
+use futures::{Future, Stream};
+use futures::future::Either;
 
 use syslog::Facility;
 
-use error::Error;
+use error::{Error, Result};
 
 use clap::{Arg, App};
+
+pub type MyFuture<'a, T> = Box<Future<Item = T, Error = Error> + 'a>;
 
 #[derive(Debug)]
 struct State {
@@ -38,27 +46,28 @@ impl Default for State {
     }
 }
 
-fn get_bridge(username: &str) -> Result<Bridge, Error> {
+fn get_bridge(core:&Core, username: &str) -> Result<Bridge> {
     let mut ips = discover_upnp()?;
     let ip = match ips.pop() {
         Some(ip) => ip,
         None => return Err("no IP found for bridge".into()),
     };
-    Ok(Bridge::new(ip, username))
+    let bridge = Bridge::new(core, ip, username);
+    Ok(bridge)
 }
 
 fn is_newly_reachable(state: &mut State, id: &str, reachable: bool, name: &str) -> bool {
     let newly = match state.reachable.get(id) {
         None => {
-            info!("new light: {}", id);
+            warn!("new light: {}", id);
             false // we don't know about this light yet...
         }
         Some(&was_reachable) => {
             if was_reachable && !reachable {
-                info!("light went off: {}", name);
+                warn!("light went off: {}", name);
             }
             if !was_reachable && reachable {
-                info!("light now on: {}", name);
+                warn!("light now on: {}", name);
             }
             !was_reachable && reachable
         }
@@ -67,11 +76,11 @@ fn is_newly_reachable(state: &mut State, id: &str, reachable: bool, name: &str) 
     newly
 }
 
-fn set_light(bridge: &Bridge, state: &State, id: &str) -> Result<(), Error> {
+fn set_light<'a>(bridge: &'a Bridge, state: &State, id: &str) -> MyFuture<'a, ()> {
     match state.lights.get(id) {
         None => {
             warn!("error: can't set light");
-            Ok(())
+            return Box::new(futures::future::ok(()))
         }
         Some(light) => {
             let cmd = LightCommand::default();
@@ -81,37 +90,46 @@ fn set_light(bridge: &Bridge, state: &State, id: &str) -> Result<(), Error> {
                 data::ColorState::Xy(ref xy) => cmd.with_xy((xy.x, xy.y)),
                 data::ColorState::Ct(ref ct) => cmd.with_ct(ct.ct).with_sat(254),
             };
-            let resps = bridge.set_light_state(light.id, &cmd)?;
-            for resp in resps {
-                info!("Response: {:?}", resp);
-            }
-            Ok(())
+            let f = bridge.set_light_state(light.id, &cmd)
+                .from_err::<Error>()
+                .and_then(|resps| {
+                    for resp in resps {
+                        warn!("Response: {:?}", resp);
+                    }
+                    futures::future::ok(())
+                });
+            return Box::new(f)
         }
-    }
+    };
 }
 
-fn handle_lights(state: &mut State, bridge: &Bridge) -> Result<(), Error> {
-    let lights = bridge.get_all_lights()?;
-    for (id, light) in lights {
-        let reachable = light.state.reachable;
-        // println!("{} {}", id, reachable);
-        let light = data::Light::make(light, id);
-        if is_newly_reachable(state, &light.uniqueid, reachable, &light.name) {
-            set_light(bridge, state, &light.uniqueid)?;
-        } else {
-            // set or update state
-            match state.lights.get(&light.uniqueid) {
-                Some(old_light) => {
-                    if *old_light != light {
-                        info!("Updating light {}", light.name)
-                    }
-                },
-                None => (),
+fn handle_lights<'a>(state: &'a mut State, bridge: &'a Bridge) -> MyFuture<'a, ()> {
+    let f = bridge.get_all_lights()
+        .from_err::<Error>()
+        .and_then(move |lights| {
+            let stream = futures::stream::iter(lights.into_iter().map(|l| Ok(l)));
+        stream.for_each(move |(id, light)| {
+            let reachable = light.state.reachable;
+            // println!("{} {}", id, reachable);
+            let light = data::Light::make(light, id);
+            if is_newly_reachable(state, &light.uniqueid, reachable, &light.name) {
+                set_light(bridge, state, &light.uniqueid)
+            } else {
+                // set or update state
+                match state.lights.get(&light.uniqueid) {
+                    Some(old_light) => {
+                        if *old_light != light {
+                            warn!("Updating light {}", light.name)
+                        }
+                    },
+                    None => (),
+                }
+                state.lights.insert(light.uniqueid.clone(), light);
+                Box::new(futures::future::ok(()))
             }
-            state.lights.insert(light.uniqueid.clone(), light);
-        }
-    }
-    Ok(())
+        })
+    });
+    Box::new(f)
 }
 
 fn main() {
@@ -132,20 +150,20 @@ fn main() {
     if matches.is_present("syslog") {
         let syslog = syslog::unix(Facility::LOG_USER).unwrap();
         log::set_logger(|max_level| {
-            max_level.set(log::LogLevelFilter::Info);
+            max_level.set(log::LogLevelFilter::Warn);
             syslog}).unwrap();
     } else {
-        env::set_var("RUST_LOG","debug");
+        env::set_var("RUST_LOG","info, hyper=warn");
         env_logger::init().unwrap(); 
     }
 
     let username = matches.value_of("username").unwrap();
 
-    let mut state = State::default();
 
+    let mut core = Core::new().unwrap();
     loop {
-        let bridge = match get_bridge(&username) {
-            Ok(bridge) => bridge,
+        let bridge = match get_bridge(&core, &username) {
+            Ok(cb) => cb,
             Err(e) => {
                 error!("Error finding bridge: {:?}", e);
                 // try to find bridge again in 60 sec
@@ -153,17 +171,38 @@ fn main() {
                 continue;
             }
         };
+        let mut state = State::default();
         loop {
-            match handle_lights(&mut state, &bridge) {
-                Ok(_) => (),
-                Err(e) => {
+            // using select2 with a sleep
+            // instead of a tokio_timer::Timer::timeout()
+            // because of timeout having an unwieldable error
+            // type signature
+            let timeout = tokio_timer::Timer::default();
+            let ts_f = timeout.sleep(Duration::new(10,0))
+                .from_err::<Error>();
+            let handle_f = handle_lights(&mut state, &bridge);
+            let future = handle_f.select2(ts_f)
+                .then(|res| {
+                    match res {
+                        Ok(Either::A(_)) => futures::future::ok(()).boxed(),
+                        Ok(Either::B(_)) => futures::future::result(Err("timeout".into())).boxed(),
+                        Err(Either::A((e, _))) => futures::future::err(e).boxed(),
+                        Err(Either::B((e, _))) => futures::future::err(e).boxed(),
+                    }
+                })
+                .and_then(|_| {
+                    let timer = tokio_timer::Timer::default();
+                    timer.sleep(Duration::new(10,0))
+                        .from_err::<Error>()
+                })
+                .or_else(|e| {
                     error!("error handling lights: {:?}", e);
                     // try to find bridge again in 60 sec
-                    std::thread::sleep(std::time::Duration::new(60, 0));
-                    break;
-                }
-            }
-            std::thread::sleep(std::time::Duration::new(10, 0));
+                    let timer = tokio_timer::Timer::default();
+                    timer.sleep(Duration::new(60,0))
+                        .from_err::<Error>()
+                });
+            core.run(future).unwrap();
         }
     }
 }
